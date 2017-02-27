5c06242f6bdc2b91220c0ea0473459d03ad48f3c
tikhonov.denis <unitrontrade@yandex.ru>
2017-02-27 17:24:44 +0300

    Таблица  с  функционалом

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     path = require('path'),
     sass = require('gulp-sass');
 
-const rootFolder = 'src';
+const rootFolder = 'work';
 const PROXY_TARGET_URL = 'http://web69.pearl.de';
 
 
