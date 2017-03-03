066402eecd097fdba084f02d5e638ceb0c20a797
tikhonov.denis <unitrontrade@yandex.ru>
2017-03-03 14:41:34 +0300

    email проект. умеет генерить теперь  инлайновые стили из обычных. jade верстка

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,46 @@
 var gulp = require('gulp'),
     browserSyncOnj = require('browser-sync'),
     path = require('path'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    gulpdata = require('gulp-data'),
+    emailBuilder = require('gulp-email-builder');
 
-const rootFolder = 'work';
+const rootFolder = 'email';
 const PROXY_TARGET_URL = 'http://web69.pearl.de';
 
-
 gulp.task('default', ['build']);
-gulp.task('build', ['scss', 'browsersync', 'watch']);
+gulp.task('build', ['jade', 'scss', 'browsersync', 'watch']);
 gulp.task('proxy', ['scss', 'proxysync', 'watch']);
+var DATA = {};
+const DATA_FILE = path.resolve(rootFolder , 'data/data.json');
+
+gulp.task('data', function() {
+    delete require.cache[require.resolve( DATA_FILE )];
+    DATA = require( DATA_FILE );
+});
+
+gulp.task('jade',['data'], function() {
+    gulp.src(rootFolder + '/jade/index.*')
+        .pipe(jade({
+            locals: DATA,
+            pretty: true,
+            basedir: path.resolve(rootFolder)
+        }))
+        .pipe(gulp.dest(rootFolder))
+});
+
+
+var emailBuilderOptions = {
+    encodeSpecialChars: true
+};
+gulp.task('emailBuilder', function() {
+    return gulp.src([rootFolder + '/*.html'])
+        .pipe(
+            emailBuilder(emailBuilderOptions).build()
+        )
+        .pipe(gulp.dest(rootFolder + '/dist'));
+});
 
 gulp.task('scss', function () {
     return gulp.src(rootFolder + '/scss/**/*.*')
@@ -46,7 +77,9 @@ gulp.task('reload',['scss'], function(){
 });
 
 gulp.task('watch',function () {
-    gulp.watch([rootFolder + '/scss/**/*.*'], ['scss','reload']);
+    gulp.watch([rootFolder + '/scss/**/*.*'], ['scss','jade','reload']);
+    gulp.watch([rootFolder + '/jade/**/*.*'], ['jade','reload']);
+    gulp.watch([rootFolder + '/data/**/*.*'],['data','jade','reload']);
     gulp.watch([rootFolder + '/*.html'], ['reload']);
     gulp.watch([rootFolder + '/image/**/*.*'], ['reload']);
     gulp.watch([rootFolder + '/css/**/*.*'], ['reload']);
