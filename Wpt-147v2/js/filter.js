30d46fc9e5cad7c9ea871a62821e33bb5e34f531
tikhonov.denis <unitrontrade@yandex.ru>
2017-03-13 14:30:36 +0300

    делаем фильтр

---
+++ Wpt-147v2/js/filter.js
@@ -0,0 +1,67 @@
+/**
+ * Created by tikhonov3 on 13.03.2017.
+ */
+const data = [
+    {
+        cost: "39.5" ,title: "Popa", img: "images/logo_ntfs.png", description: "dfgdfgdfg dfg dfg gwerkgolwerg ergwg", linkInfo: "#", linkBuy: "#", filter: {
+        Virtualization: true,
+        Partitioning: true,
+        Backup: true,
+        Migration: true,
+        Restore: true,
+        Maintenance: true,
+        Bundles: true,
+        Optimization: true,
+        Security: true,
+        OS: true,
+        interopterability: true,
+        Windows: true,
+        OSX: true,
+        Linux: true,
+        Freeware: true
+    }
+    },
+    {
+        cost: "139.5" , title: "", img: "", description: "", linkInfo: "#", linkBuy: "", filter: {
+        Virtualization: true,
+        Partitioning: true,
+        Backup: true,
+        Migration: true,
+        Restore: true,
+        Maintenance: true,
+        Bundles: true,
+        Optimization: true,
+        Security: true,
+        OS: true,
+        interopterability: true,
+        Windows: true,
+        OSX: true,
+        Linux: true,
+        Freeware: true
+    }
+    },
+    {
+        cost: "5639.5" ,title: "", img: "", description: "", linkInfo: "#", linkBuy: "", filter: {
+        Virtualization: true,
+        Partitioning: true,
+        Backup: true,
+        Migration: true,
+        Restore: true,
+        Maintenance: true,
+        Bundles: true,
+        Optimization: true,
+        Security: true,
+        OS: true,
+        interopterability: true,
+        Windows: true,
+        OSX: true,
+        Linux: true,
+        Freeware: true
+    }
+    }
+
+
+];
+
+
+console.log(data);
