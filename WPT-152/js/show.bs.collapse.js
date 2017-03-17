09bac176cc5ff73a3932f37a94dd4a2ef4210001
tikhonov.denis <unitrontrade@yandex.ru>
2017-03-17 19:41:32 +0300

    accordion begin

--- WPT-152/js/show.bs.collapse.js
+++ WPT-152/js/show.bs.collapse.js
@@ -1,6 +1,10 @@
 /**
  * Created by tikhonov3 on 17.03.2017.
  */
-$('.collapse').on('show.bs.collapse', function() {
-    $('.collapse.in').collapse('hide');
-});
+// $('.collapse')
+//     .on('show.bs.collapse', function() {
+//     $('.collapse.in').collapse('hide');
+// });
+// $('#accordion').collapse({
+//     toggle: false
+// })
