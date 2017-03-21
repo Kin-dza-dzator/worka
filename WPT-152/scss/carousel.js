5c1bd43e8304dd11211486ff45a1fefe535c77be
tikhonov.denis <unitrontrade@yandex.ru>
2017-03-21 17:59:00 +0300

    довнлоадер Нужен  колапсирующий  список  в  мобильном  экране Не  смог  настроить  корректно  h4    бекраунд  зебра  нужны  паддинги Забить  бордеры  на  медиа

---
+++ WPT-152/scss/carousel.js
@@ -0,0 +1,6 @@
+/**
+ * Created by tikhonov3 on 20.03.2017.
+ */
+#reviews .bx-wrapper .bx-prev {
+    background-image: url(https://www.paragon-software.com/sites/paragonsoftware.com/images/optimized/btn-prev-white.png);
+}
